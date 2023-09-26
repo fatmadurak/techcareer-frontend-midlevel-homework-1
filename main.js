a8e685edd56538f35276fbfa79ccc3b8b9561561
fatmadurak <fatmabadillidurak@gmail.com>
2023-09-26 18:05:00 +0300

    wrapper js scrolled

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const wrapper = document.querySelector(".wrapper");
 
-
-
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 60) {
+    wrapper.classList.add("scrolled");
+  } else {
+    wrapper.classList.remove("scrolled");
+  }
+});
