7fc148404fccd19a66eb87f17bde6d281274fcf4
fatmadurak <fatmabadillidurak@gmail.com>
2023-09-27 16:38:14 +0300

    formatting

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-//add scroll class 
+//add scroll class
 const wrapper = document.querySelector(".wrapper");
 
 window.addEventListener("scroll", () => {
@@ -10,67 +9,51 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
 //add scroll effect
 
-
-
-document.querySelector('.nav-brand').addEventListener("click",(e)=>{
-
-
+document.querySelector(".nav-brand").addEventListener("click", (e) => {
   e.preventDefault();
-  document.querySelector(".hero-section").scrollIntoView({behavior:"smooth"});
-  
-  
-  })
+  document
+    .querySelector(".hero-section")
+    .scrollIntoView({ behavior: "smooth" });
+});
 
-document.querySelector('a[href="#portfolio"]').addEventListener("click",(e)=>{
-
-
-e.preventDefault();
-document.querySelector(".portfolio-section").scrollIntoView({behavior:"smooth"});
-
-
-})
-
-document.querySelector('a[href="#about"]').addEventListener("click",(e)=>{
-
-
-  e.preventDefault();
-  document.querySelector(".about-section").scrollIntoView({behavior:"smooth"});
-  
-  
-  })
-
-  document.querySelector('a[href="#contact"]').addEventListener("click",(e)=>{
-
-
+document
+  .querySelector('a[href="#portfolio"]')
+  .addEventListener("click", (e) => {
     e.preventDefault();
-    document.querySelector(".contact-section").scrollIntoView({behavior:"smooth"});
-    
-    
-    })
+    document
+      .querySelector(".portfolio-section")
+      .scrollIntoView({ behavior: "smooth" });
+  });
 
+document.querySelector('a[href="#about"]').addEventListener("click", (e) => {
+  e.preventDefault();
+  document
+    .querySelector(".about-section")
+    .scrollIntoView({ behavior: "smooth" });
+});
 
- //active class
- 
- 
- const portfolio=document.querySelector("#portfolio");
- const about=document.querySelector("#about");
- const contact=document.querySelector("#contact");
+document.querySelector('a[href="#contact"]').addEventListener("click", (e) => {
+  e.preventDefault();
+  document
+    .querySelector(".contact-section")
+    .scrollIntoView({ behavior: "smooth" });
+});
 
+//active class
 
+const portfolio = document.querySelector("#portfolio");
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
 
- function setActive(element) {
-
+function setActive(element) {
   portfolio.classList.remove("active");
   about.classList.remove("active");
   contact.classList.remove("active");
 
-  
   element.classList.add("active");
 }
-
 
 portfolio.addEventListener("click", function () {
   setActive(portfolio);
