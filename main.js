687230f6fbf7611ed20c5d11fb3bff36fbab4e1d
fatmadurak <fatmabadillidurak@gmail.com>
2023-09-27 13:13:56 +0300

    added scroll effect

--- main.js
+++ main.js
@@ -9,3 +9,34 @@ window.addEventListener("scroll", () => {
     wrapper.classList.remove("scrolled");
   }
 });
+
+
+//add scroll effect
+
+
+document.querySelector('a[href="#portfolio"]').addEventListener("click",(e)=>{
+
+
+e.preventDefault();
+document.querySelector(".portfolio-section").scrollIntoView({behavior:"smooth"});
+
+
+})
+
+document.querySelector('a[href="#about"]').addEventListener("click",(e)=>{
+
+
+  e.preventDefault();
+  document.querySelector(".about-section").scrollIntoView({behavior:"smooth"});
+  
+  
+  })
+
+  document.querySelector('a[href="#contact"]').addEventListener("click",(e)=>{
+
+
+    e.preventDefault();
+    document.querySelector(".contact-section").scrollIntoView({behavior:"smooth"});
+    
+    
+    })
